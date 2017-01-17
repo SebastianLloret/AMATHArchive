eb5a4762f56c5af448d19112d74873c779a324e4
Sebastian Lloret <sebastian@lloret.io>
2017-01-17 16:13:27 -0700

    Person functionality

--- scraper.js
+++ scraper.js
@@ -6,10 +6,21 @@ request('https://www.colorado.edu/amath/2016/10/27/complexdynamical-systems-semi
     var $ = cheerio.load(html);
     var dates = [];
     var person = [];
+
     // Dates
     // Grab all text inside of elements with the class .author-meta, split at
     // ':', and remove whitespace at the beginning/end of the string.
     dates.push($('.author-meta').text().split(':')[1].trim());
-    console.log(dates);
+
+    // Person
+    // Grab the contents of the first data table, and return anything
+    // with the type of 'text'
+    person.push($('td').first().contents().filter(
+      function() {
+        return this.type === 'text';
+      }
+    ).text().trim());
+
+    console.log(person);
   }
 });
