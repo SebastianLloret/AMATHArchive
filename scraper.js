89e7af7ef20e101ac019db1e9ee8f279f832fc19
Sebastian Lloret <sebastian@lloret.io>
2017-01-24 15:12:03 -0700

    Project complete

--- scraper.js
+++ scraper.js
@@ -1,14 +1,15 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var date = [];
-var person = [];
-var type = [];
+var async = require('async');
 
-function requestSite(p) {
-  request('http://www.colorado.edu/amath/events/archived-events?page=' + p, function(error, response, html) {
+var q = async.queue(function (task, done) {
+  request(task.url, function(error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
-      console.log('Running');
+      var date = [];
+      var person = [];
+      var title = [];
+
       // Dates
       $('.date').each(function () {
         date.push($(this).text().trim());
@@ -18,26 +19,35 @@ function requestSite(p) {
       $('.node-title').each(function(){
         if ($(this).text().indexOf('-') >= 0) {
           var toSplit = $(this).text().split('-');
-          type.push(toSplit[0].trim());
+          title.push(toSplit[0].trim());
           person.push(toSplit[1].trim());
         }
 
         else if($(this).text().indexOf(':') >= 0) {
           var toSplit = $(this).text().split(':');
-          type.push(toSplit[0].trim());
+          title.push(toSplit[0].trim());
           person.push(toSplit[1].trim());
         }
 
         else {
-          type.push($(this).text().trim());
+          title.push($(this).text().trim());
           person.push('');
         }
       });
-    }
-  });
-}
 
-for (var p = 0; p < 42; p++) {
-  requestSite(p);
-  console.log(p);
+      for (var i = 0; i < date.length; i++) {
+        if (person[i] == ''){
+          console.log(date[i] + ' - \"' + title[i] + '\"')
+        }
+        else {
+          console.log(date[i] + ' - ' + person[i] + ' \"' + title[i] + '\"');
+        }
+      }
+    }
+    done();
+  });
+}, 5);
+
+for (var i = 0; i < 43; i++){
+  q.push({ url: 'http://www.colorado.edu/amath/events/archived-events?page=' + i});
 }
